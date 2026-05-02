189927c62673da961ff5b423979cd80254b3c70e
Andreas Mukombambi <mukombambia@gmail.com>
2026-05-02 15:38:30 +0200

    9

--- src/components/ExplorePage.tsx
+++ src/components/ExplorePage.tsx
@@ -582,7 +582,7 @@ const ExplorePage = ({ initialDestinationId: _initialDestinationId }: ExplorePag
   }
 
   return (
-    <main className="fixed inset-x-0 bottom-0 top-[calc(0px_-_env(safe-area-inset-top))] overflow-hidden bg-background text-foreground">
+    <main className="fixed inset-x-0 bottom-[calc(0px_-_env(safe-area-inset-bottom))] top-[calc(0px_-_env(safe-area-inset-top))] overflow-hidden bg-background text-foreground">
       {/* Map */}
       <div className={["absolute inset-0", showDesktopTripPanel ? "lg:left-96" : ""].join(" ")}>
         <MapboxStreetsMap
